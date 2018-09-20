ff11c7e4494b07997a4f22441a9dcf8b020c8023
Andrea Ranfone <ranfone.andrea@gmail.com>
2018-09-20 11:17:53 +0200

    aggiunta comunicazione rest con il nodo fog

--- src/main/java/MiddlewareProject/handler/TaskHandler.java
+++ src/main/java/MiddlewareProject/handler/TaskHandler.java
@@ -1,6 +1,7 @@
 package MiddlewareProject.handler;
 
 import MiddlewareProject.task.*;
+import MiddlewareProject.utils.JsonBuilder;
 
 import java.util.ArrayList;
 
@@ -10,11 +11,16 @@ public class TaskHandler {
 
     private static TaskHandler ourInstance = new TaskHandler();
 
+    private JsonBuilder jsonBuilder;
+    private RequestHandler requestHandler;
+
     public static TaskHandler getInstance() {
         return ourInstance;
     }
 
     private TaskHandler() {
+        jsonBuilder = new JsonBuilder();
+        requestHandler = new RequestHandler();
     }
 
     public ArrayList<MiddlewareTask> getTaskList() {
@@ -24,19 +30,37 @@ public class TaskHandler {
     public void addLightTask(LightTask task){
         MiddlewareTask middlewareTask = new MiddlewareTask(task);
         taskList.add(middlewareTask);
-        printList();
+        //printList();
     }
 
     public void addMediumTask(MediumTask task){
         MiddlewareTask middlewareTask = new MiddlewareTask(task);
         taskList.add(middlewareTask);
-        printList();
+        //printList();
     }
 
     public void addHeavyTask(HeavyTask task){
         MiddlewareTask middlewareTask = new MiddlewareTask(task);
         taskList.add(middlewareTask);
-        printList();
+        //printList();
+    }
+
+    public String sendLightTask (LightTask lightTask){
+        String payload = jsonBuilder.LightTaskToJSON(lightTask);
+        String requestUrl="http://localhost:8090/light";
+        return requestHandler.sendPostRequest(requestUrl, payload);
+    }
+
+    public String sendMediumTask(MediumTask mediumTask){
+        String payload = jsonBuilder.MediumTaskToJSON(mediumTask);
+        String requestUrl="http://localhost:8090/medium";
+        return requestHandler.sendPostRequest(requestUrl, payload);
+    }
+
+    public String sendHeavyTask(HeavyTask heavyTask){
+        String payload = jsonBuilder.HeavyTaskToJSON(heavyTask);
+        String requestUrl="http://localhost:8090/heavy";
+        return requestHandler.sendPostRequest(requestUrl, payload);
     }
 
     private void printList(){
--- src/main/java/MiddlewareProject/rest/HeavyTaskService.java
+++ src/main/java/MiddlewareProject/rest/HeavyTaskService.java
@@ -2,7 +2,6 @@ package MiddlewareProject.rest;
 
 import MiddlewareProject.handler.TaskHandler;
 import MiddlewareProject.task.HeavyTask;
-import MiddlewareProject.task.Task;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,17 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(path = "heavy")
 public class HeavyTaskService {
 
     @RequestMapping(path = "", method = RequestMethod.POST)
-    public ResponseEntity<String> solveHeavyTask(@RequestBody HeavyTask heavyTask) {
+    public ResponseEntity<String> solveHeavyTask(@RequestBody HeavyTask heavyTask,HttpServletRequest request) {
 
-        TaskHandler.getInstance().addHeavyTask(heavyTask);
-        return new ResponseEntity<>("ACK", HttpStatus.OK);
+        //System.out.println(request.getRemoteAddr());
+        TaskHandler taskHandler = TaskHandler.getInstance();
+        taskHandler.addHeavyTask(heavyTask);
+        String res = taskHandler.sendHeavyTask(heavyTask);
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
 }
