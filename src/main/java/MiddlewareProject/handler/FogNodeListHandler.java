c7806541f624c71a5a36a70bc0efcca9bfb8006a
Andrea Cifola <cifola.andrea@gmail.com>
2018-09-21 11:13:59 +0200

    Aggiunte alcune modifiche

--- src/main/java/MiddlewareProject/handler/FogNodeListHandler.java
+++ src/main/java/MiddlewareProject/handler/FogNodeListHandler.java
@@ -1,13 +1,13 @@
 package MiddlewareProject.handler;
 
-
 import MiddlewareProject.entities.FogNode;
-
+import MiddlewareProject.utils.Requests;
 import java.util.ArrayList;
+
 
 public class FogNodeListHandler {
 
-    RequestHandler requestHandler = new RequestHandler();
+    private Requests requests = new Requests();
 
     public void checkAlivesFogNodes() {
         new Thread(new Runnable() {
@@ -16,13 +16,16 @@ public class FogNodeListHandler {
                 while (true) {
                     try {
                         Thread.sleep(3000);
-                    } catch (InterruptedException e) {
+                        ArrayList<FogNode> aliveFogNodes = RegistrationHandler.getInstance().getArrayListFogNode();
+                        for (FogNode aliveFogNode : aliveFogNodes) {
+                            StringBuilder jsonString = new StringBuilder();
+                            String requestUrl = "http://localhost:" +
+                                    aliveFogNode.getPort() +
+                                    aliveFogNode.getId();
+                            requests.sendPostRequest(requestUrl, "Are you alive?", jsonString);
+                        }
+                    } catch (Exception e) {
                         e.printStackTrace();
-                    }
-                    ArrayList<FogNode> aliveFogNodes = RegistrationHandler.getInstance().getArrayListFogNode();
-                    for (int i = 0; i < aliveFogNodes.size(); i++) {
-
-
                     }
                 }
             }
--- src/main/java/MiddlewareProject/rest/RegistrationService.java
+++ src/main/java/MiddlewareProject/rest/RegistrationService.java
@@ -9,14 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping(path = "registration")
 public class RegistrationService {
 
     @RequestMapping(path = "", method = RequestMethod.POST)
-    public ResponseEntity<FogNode> fogNodeRegistration(@RequestBody FogNode fogNode) {
+    public ResponseEntity<FogNode> fogNodeRegistration(@RequestBody FogNode fogNode, HttpServletRequest request) {
 
         FogNode updatedFogNode = RegistrationHandler.getInstance().addNodeToNodeList(fogNode);
+
+        updatedFogNode.setPort(String.valueOf(request.getRemotePort()-1));
+        System.out.println(updatedFogNode.getPort());
+
         return new ResponseEntity<>(updatedFogNode, HttpStatus.OK);
     }
 
